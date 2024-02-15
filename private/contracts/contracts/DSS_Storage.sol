fc535360a7566d416bb020ed3f3f14c94b563901
Daniel Cavassani <daniel_cavassani@hotmail.com>
2024-02-15 13:29:10 -0300

    Refactor DSS_Storage contract for optimized operations and improved permission management
    - Implemented upsertPolygonData function to unify adding and updating of ChunkData, simplifying polygon manipulation logic.
    - Enhanced data deletion efficiency with deletePolygonData function, enabling batch removal of ChunkData based on IDs.
    - Improved permission checks, ensuring only the creator (addedBy) can update or delete specific ChunkData, enhancing contract security.
    - Introduced optimizations in getData function for efficient data retrieval based on geohash filters, height, and time intervals.
    - Reorganized and cleaned up the code to improve readability and maintenance, including updating modifiers and structuring events and structs.
    - Removed obsolete functions and unused code, reducing complexity and potential gas costs.
    
    This update aims to enhance the usability and efficiency of the DSS_Storage contract, focusing on the flexibility of data operations and the robustness of authorization checks. Extensive testing has been conducted to ensure the integrity of functionalities post-refactor.

--- private/contracts/contracts/DSS_Storage.sol
+++ private/contracts/contracts/DSS_Storage.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract DSS_Storage {
+    /* EVENTS */
+
     // Events defined to track data additions, updates, and deletions
     event DataAdded(uint indexed id, string geohash, address indexed addedBy);
     event DataUpdated(uint indexed id, string geohash, address indexed updatedBy);
     event DataDeleted(uint indexed id, string geohash, address indexed deletedBy);
 
-    // Structs to store the data
+
+    /* STRUCTS */
     struct HeightInterval {
         uint min;
         uint max;
@@ -21,7 +24,7 @@ contract DSS_Storage {
     struct ResourceInfo {
         string url;
         uint entityNumber;
-        uint id;
+        uint id;                // id must be greater than 0
     }
 
     struct ChunkData {
@@ -31,21 +34,20 @@ contract DSS_Storage {
         ResourceInfo resourceInfo;
     }
 
-    // Mapping from geohash to an array of ChunkData
-    mapping(string => ChunkData[]) public geohashToChunkDataArray;
 
-    // State to store allowed users
-    mapping(address => bool) public allowedUsers;
+    /* INDEXES */
 
-    // Contract owner address
-    address public owner;
+    // Here, only the active data must be stored. Inactive data must be deleted from the both mappings.
+    // OBS: IDs that was deleted will be considered as inexistent in the future, so it's not necessary to store them in a separate mapping. If necessary, users can reuse the same ID, but it's not recommended.
+    mapping(uint => string[]) public idToGeohash;                       // Mapping from id to geohash
+    mapping(string => ChunkData[]) public geohashToChunkDataArray;      // Mapping from geohash to an array of ChunkData (don't need to be an ordered aray)
 
-    // Constructor to set the deployer as the owner
-    constructor() {
-        // Defining the deployer as owner
-        owner = msg.sender;
-        allowUser(owner);
-    }
+    mapping(address => bool) public allowedUsers;       // State to store allowed users
+
+    address public owner;       // Contract owner address
+
+
+    /* MODIFIERS */
 
     // Modifier to restrict calls only by allowed users
     modifier onlyAllowed {
@@ -69,61 +71,189 @@ contract DSS_Storage {
         allowedUsers[_user] = false;
     }
 
-    // Function to add a Batch of new ChunkData
-    function insertDataBatch (
-        string[] memory _geohashes,
-        uint[] memory _minHeights,
-        uint[] memory _maxHeights,
-        uint[] memory _startTimes,
-        uint[] memory _endTimes,
-        string[] memory _urls,
-        uint[] memory _entities,
-        uint[] memory _ids
-    ) 
-        public onlyAllowed
-    {
-        require(
-            _geohashes.length == _minHeights.length &&
-            _geohashes.length == _maxHeights.length &&
-            _geohashes.length == _startTimes.length &&
-            _geohashes.length == _endTimes.length &&
-            _geohashes.length == _urls.length &&
-            _geohashes.length == _entities.length &&
-            _geohashes.length == _ids.length,
-            "Input arrays must have the same length"
-        );
-
-        for (uint i = 0; i < _geohashes.length; i++) {
-            insertSingleData(_geohashes[i], _minHeights[i], _maxHeights[i], _startTimes[i], _endTimes[i], _urls[i], _entities[i], _ids[i]);
-        }
+    // Function to change the owner of the contract
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
     }
 
-    // Function to add new ChunkData
-    function insertSingleData (
-        string memory _geohash, 
-        uint _minHeight, 
-        uint _maxHeight, 
+
+    /* CONSTRUCTOR */
+
+    // Constructor to set the deployer as the owner
+    constructor() {
+        owner = msg.sender;
+        allowUser(owner);
+    }
+
+
+    /* UTILS */
+
+    // no functions needed to be implemented until now
+
+
+    /* UPSERT */
+
+    // Function to add/update a new Polygon, which is a set of geohashes and its corresponding ChunkData
+    // Here, one polygon is a part of a full route
+    function upsertPolygonData (
+        string[] memory _geohashes,
+        uint _minHeight,
+        uint _maxHeight,
         uint _startTime,
         uint _endTime,
-        string memory _url, 
-        uint _entity, 
+        string memory _url,
+        uint _entity,
         uint _id
     ) 
         public onlyAllowed
     {
+        require(_geohashes.length > 0, "No geohashes provided");
         require(_maxHeight >= _minHeight, "Max height must be greater than or equal to min height");
         require(_startTime < _endTime, "Start time must be less than end time");
 
+        // Create the necessary structs to compose the ChunkData to be added to the geohashToChunkDataArray mapping
         HeightInterval memory height = HeightInterval({min: _minHeight, max: _maxHeight});
         TimeInterval memory time = TimeInterval({start: _startTime, end: _endTime});
         ResourceInfo memory resourceInfo = ResourceInfo({url: _url, entityNumber: _entity, id: _id});
-        ChunkData memory newChunkData = ChunkData({addedBy: msg.sender, height: height, time: time, resourceInfo: resourceInfo});
+        ChunkData memory newChunkData = ChunkData({addedBy: msg.sender, height: height, time: time, resourceInfo: resourceInfo});   // Create the new ChunkData
 
-        emit DataAdded(_id, _geohash, msg.sender);
-        geohashToChunkDataArray[_geohash].push(newChunkData);
+        // Map old geohashes for the given id. Needed to update and delete the old geohashes that are not in the new polygon
+        string[] memory oldGeohashes = idToGeohash[_id];                        // get the old geohashes for the given id
+        bool[] memory oldGeohashesUpdated = new bool[](oldGeohashes.length);    // temporary array to mark the updated geohashes in runtime (refers to the old geohashes)
+
+        // Update and insert new geohashes, depending if they are already in the old polygon or not (collision check)
+        for (uint i = 0; i < _geohashes.length; i++) {      // Iterate over new hashes for the given id
+            string memory currentNewGeohash = _geohashes[i];
+            bool exists = false;
+
+            for (uint j = 0; j < oldGeohashes.length; j++) {    // Iterate over old hashes for the given id
+                string memory currentOldGeohash = oldGeohashes[j];
+
+                // Update the old geohashes that are in the new polygon
+                if (keccak256(abi.encodePacked(currentNewGeohash)) == keccak256(abi.encodePacked(currentOldGeohash))) {
+                    exists = true;
+                    oldGeohashesUpdated[j] = true; // Mark the updated geohash in the temporary array
+                    updateChunkData(currentNewGeohash, newChunkData);
+                    break;
+                }
+            }
+
+            // Insert the new geohashes that are not in the old polygon
+            if (!exists) {
+                insertChunkData(currentNewGeohash, newChunkData);
+            }
+        }
+
+        // Delete the old geohashes that are not in the new polygon
+        for (uint i = 0; i < oldGeohashes.length; i++) {
+            if (!oldGeohashesUpdated[i]) {
+                deleteChunkData(_id, oldGeohashes[i]);
+            }
+        }
     }
 
-    // Function to return multiple fields of the first matching ChunkData
+
+    /* INSERT */
+
+    // Function to add new ChunkData
+    function insertChunkData (
+        string memory _geohash, 
+        ChunkData memory _chunkData
+    ) 
+        private onlyAllowed
+    {
+        uint currentId = _chunkData.resourceInfo.id;
+
+        // Add the id to the idToGeohash mapping and the ChunkData to the geohashToChunkDataArray mapping
+        idToGeohash[currentId].push(_geohash);
+        geohashToChunkDataArray[_geohash].push(_chunkData);
+        emit DataAdded(currentId, _geohash, msg.sender);
+    }
+
+
+    /* UPDATE */
+
+    // Function to update a single ChunkData
+    function updateChunkData(
+        string memory _geohash, 
+        ChunkData memory _chunkData
+    ) 
+        private onlyAllowed
+    {
+        if (_chunkData.addedBy != msg.sender) {
+            return;
+        }
+
+        uint currentId = _chunkData.resourceInfo.id;
+        require(idToGeohash[currentId].length > 0, "No data to be updated for the given id");
+
+        // Iterate over the array of ChunkData for the given geohash and update the data for the given id
+        ChunkData[] storage currentChunkDataArray = geohashToChunkDataArray[_geohash];
+        for (uint i = 0; i < currentChunkDataArray.length; i++) {
+            if (currentChunkDataArray[i].resourceInfo.id == currentId) {
+                currentChunkDataArray[i].height = _chunkData.height;
+                currentChunkDataArray[i].time = _chunkData.time;
+                currentChunkDataArray[i].resourceInfo = _chunkData.resourceInfo;
+                break;
+            }
+        }
+        
+        emit DataUpdated(currentId, _geohash, msg.sender);
+    }
+
+
+    /* DELETE */
+
+    // Function to delete a Batch of ChunkData by its ids
+    function deletePolygonData(uint[] memory _ids) public onlyAllowed {
+        require(_ids.length > 0, "No ids provided");
+
+        // Delete the ChunkData for each geohash for each id
+        for (uint i = 0; i < _ids.length; i++) {
+            uint currentId = _ids[i];
+            string[] memory currentGeohashes = idToGeohash[currentId];
+
+            for (uint j = 0; j < currentGeohashes.length; j++) {
+                deleteChunkData(currentId, currentGeohashes[j]);
+            }
+        }
+    }
+
+    // Function to remove a ChunkData from the array of a given geohash by its id
+    function deleteChunkData(uint _id, string memory _geohash) private {
+        ChunkData[] storage currentChunkDataArray = geohashToChunkDataArray[_geohash];
+        string[] storage currentGeohashes = idToGeohash[_id];
+
+        // Delete from geohashToChunkDataArray the specific ChunkData
+        for (uint i = 0; i < currentChunkDataArray.length; i++) {
+            ChunkData storage currentChunkData = currentChunkDataArray[i];
+            if (currentChunkData.resourceInfo.id == _id) {
+                if (currentChunkData.addedBy != msg.sender) {
+                    return;
+                }
+                currentChunkData = currentChunkDataArray[currentChunkDataArray.length - 1]; // Move the last element to the position of the element to be deleted
+                currentChunkDataArray.pop();    // Delete the last element
+                break;
+            }
+        }
+
+        // Delete from idtoGeohash mapping the specific geohash
+        for (uint i = 0; i < currentGeohashes.length; i++) {
+            string storage currentGeohash = currentGeohashes[i];
+            if (keccak256(abi.encodePacked(currentGeohash)) == keccak256(abi.encodePacked(_geohash))) {
+                currentGeohash = currentGeohashes[currentGeohashes.length - 1]; // Move the last element to the position of the element to be deleted
+                currentGeohashes.pop();    // Delete the last element
+                break;
+            }
+        }
+
+        emit DataDeleted(_id, _geohash, msg.sender);
+    }
+
+
+    /* RETRIEVE */
+
+    // Function to retrieve data for a given geohash, height interval and time interval. Parameters works as filters.
     function getData(
         string memory _geohash, 
         uint _minHeight, 
@@ -173,32 +303,6 @@ contract DSS_Storage {
         }
 
         return (urls, entityNumbers, ids);
-    }
-
-    // Function to remove a ChunkData from the array of a given geohash by its id
-    function deleteData(string memory _geohash, uint _id) public onlyAllowed {
-        require(geohashToChunkDataArray[_geohash].length > 0, "No data for the given geohash");
-        require(_id > 0, "Id must be greater than 0");
-
-        // Get the array of ChunkData for the given geohash
-        ChunkData[] storage currentChunkDataArray = geohashToChunkDataArray[_geohash];
-
-        // Find the index of the element to be deleted
-        uint i;
-        for (i = 0; i < currentChunkDataArray.length; i++) {
-            if (currentChunkDataArray[i].resourceInfo.id == _id) {
-                break;
-            }
-        }
-        require(i < currentChunkDataArray.length, "No data for the given id");
-
-        emit DataDeleted(_id, _geohash, msg.sender);
-
-        // Move the last element to the position of the element to be deleted
-        currentChunkDataArray[i] = currentChunkDataArray[currentChunkDataArray.length - 1];
-
-        // Delete the last element
-        currentChunkDataArray.pop();
     }
 
     // Revert fallback and receive functions
--- private/startDev.sh
+++ private/startDev.sh
@@ -119,7 +119,6 @@ sleep 5
 
 echo "Deploying contract..."
 
-truffle migrate --f 3 --to 3 --network development
-truffle migrate --f 5 --to 5 --network development
+truffle migrate --f 1 --to 5 --network development
 
 # Nota: O truffle está sendo descontinuado, então é bom mudar para um Hardhat ou algo do tipo no futuro
